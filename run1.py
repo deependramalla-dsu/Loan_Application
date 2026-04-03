8af245166f39f2d23bbdf6b5b2736c62e08d3fd5
deependramalla-dsu <deependra.malla@gmail.com>
2026-04-02 23:02:44 -0400

    Update run1.py

--- run1.py
+++ run1.py
@@ -27,13 +27,10 @@ if app_mode=='Home':
     st.write(data.head()) 
     st.markdown('Applicant Income VS Loan Amount ')
     st.bar_chart(data[['ApplicantIncome','LoanAmount']].head(20))
-   
-   
-   
 elif app_mode =='Prediction':
     
-    csv=pd.read_csv("test.csv")
-    st.write(csv)
+    #csv=pd.read_csv("test.csv")
+    #st.write(csv)
 
     #st.image('slider-short-3.jpg')
 
@@ -43,19 +40,18 @@ elif app_mode =='Prediction':
     feature_dict = {"No":1,"Yes":2}
     edu={'Graduate':1,'Not Graduate':2}
     prop={'Rural':1,'Urban':2,'Semiurban':3}
-    Gender=st.sidebar.radio('Gender',tuple(gender_dict.keys()))
-    Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))
-    Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))
-    Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])
-    Education=st.sidebar.radio('Education',tuple(edu.keys()))
     ApplicantIncome=st.sidebar.slider('ApplicantIncome',0,10000,0,)
     CoapplicantIncome=st.sidebar.slider('CoapplicantIncome',0,10000,0,)
     LoanAmount=st.sidebar.slider('LoanAmount in K$',9.0,700.0,200.0)
     Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,240.0,300.0,360.0))
     Credit_History=st.sidebar.radio('Credit_History',(0.0,1.0))
+    Gender=st.sidebar.radio('Gender',tuple(gender_dict.keys()))
+    Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))
+    Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))
+    Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])
+    Education=st.sidebar.radio('Education',tuple(edu.keys()))
     Property_Area=st.sidebar.radio('Property_Area',tuple(prop.keys()))
-
-
+    
     class_0 , class_3 , class_1,class_2 = 0,0,0,0
     if Dependents == '0':
         class_0 = 1
@@ -88,7 +84,24 @@ elif app_mode =='Prediction':
     'Property_Area':[Rural,Urban,Semiurban],
     }
 
-    feature_list=[ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,get_value(Gender,gender_dict),get_fvalue(Married),data1['Dependents'][0],data1['Dependents'][1],data1['Dependents'][2],data1['Dependents'][3],get_value(Education,edu),get_fvalue(Self_Employed),data1['Property_Area'][0],data1['Property_Area'][1],data1['Property_Area'][2]]
+    feature_list=[
+                ApplicantIncome,
+                CoapplicantIncome,
+                LoanAmount,
+                Loan_Amount_Term,
+                Credit_History,
+                get_value(Gender,gender_dict),
+                get_fvalue(Married),
+                data1['Dependents'][0],
+                data1['Dependents'][1],
+                data1['Dependents'][2],
+                data1['Dependents'][3],
+                get_value(Education,edu),
+                get_fvalue(Self_Employed),
+                data1['Property_Area'][0],
+                data1['Property_Area'][1],
+                data1['Property_Area'][2]
+                ]
 
     single_sample = np.array(feature_list).reshape(1,-1)
 
